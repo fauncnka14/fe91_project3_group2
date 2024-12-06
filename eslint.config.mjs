1160425f44d28fa2e94ae0e6716be74add22b6c8
missirina1 <erina201497@bk.ru>
2024-12-06 18:36:58 +0500

    Добавление статистических анализов кода и JSON сервера

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from 'globals'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    parserOptions: {
+      sourceType: 'module', // Поддержка ES-модулей
+    },
+    ignores: ['node_modules/**', 'dist/**'], // Игнорировать ненужные папки
+  },
+  {
+    files: ['**/*.js'], // Применяется к JS-файлам
+    languageOptions: {
+      sourceType: 'module', // Поддержка ES-модулей
+      globals: globals.browser, // Глобальные переменные браузера
+    },
+    rules: {
+      'no-unused-vars': 'warn', // Предупреждение для неиспользуемых переменных
+      'no-console': 'off', // Разрешить использование console.log
+    },
+  },
+]
